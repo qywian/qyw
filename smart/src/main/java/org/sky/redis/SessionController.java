86c54c38a6d3bc9f16b6a9319eeb4db49710c0ad
qywian@126.com <qywian@126.com>
2016-07-26 13:00:53 +0800

    新建项目

--- smart/src/main/java/org/sky/redis/SessionController.java
+++ smart/src/main/java/org/sky/redis/SessionController.java
@@ -1,11 +1,23 @@
 package org.sky.redis;
 
-import org.springframework.web.servlet.DispatcherServlet;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableRedisHttpSession
 public class SessionController {
-	
-	public static void main(String[] args) {
-		DispatcherServlet
+	@RequestMapping("/mySession")
+	public String index(final Model model,final HttpServletRequest request){
+		if(request.getSession().getAttribute("testSession") == null){
+			System.out.println("session is null ");
+			request.getSession().setAttribute("testSession", "year");
+		}else{
+			System.out.println("session is not null");
+		}
+		return "showSession";
 	}
-
 }
