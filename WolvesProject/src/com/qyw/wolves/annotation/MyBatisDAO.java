96d2c179904c99e6528489928e8a3b916c761459
qywian@126.com <qywian@126.com>
2016-09-08 17:59:15 +0800

    更新

---
+++ WolvesProject/src/com/qyw/wolves/annotation/MyBatisDAO.java
@@ -0,0 +1,24 @@
+package com.qyw.wolves.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+/**
+ * 标识MyBatis的DAO,方便{@link org.mybatis.spring.mapper.MapperScannerConfigurer}的扫描。 
+ * @author qyw
+ * @version 2016-9-8
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Component
+public @interface MyBatisDAO {
+	
+	String value() default "";
+
+}
